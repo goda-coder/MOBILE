098c125aa4fe42ff5743fd56b828e33b94ea785c
MohamedAboSeada <madoaxel287@gmail.com>
2026-06-23 15:20:04 +0300

    changes نؤطة

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/api_models.dart';
 import '../state/providers.dart';
